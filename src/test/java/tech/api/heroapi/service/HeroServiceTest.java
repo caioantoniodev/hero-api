3376f25f0e74553225d00e1856a93265bbdb437e
Caio Antonio <caio.antonio.c@outlook.com>
2024-03-19 22:35:41 -0300

    added test for get heroes

--- src/test/java/tech/api/heroapi/service/HeroServiceTest.java
+++ src/test/java/tech/api/heroapi/service/HeroServiceTest.java
@@ -5,10 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import tech.api.heroapi.entity.Heroes;
+import tech.api.heroapi.entity.enums.HeroAlignmentEnum;
 import tech.api.heroapi.kafka.model.HeroCreateEventModel;
 import tech.api.heroapi.kafka.producer.HeroCreateEventProducer;
 import tech.api.heroapi.repository.HeroRepository;
 import tech.api.heroapi.rest.controller.model.HeroRequest;
+import tech.api.heroapi.rest.controller.model.HeroResponse;
+
+import java.util.List;
+import java.util.UUID;
 
 import static org.mockito.Mockito.*;
 
@@ -41,5 +47,22 @@ public class HeroServiceTest {
         var heroCreateEventModel = heroCreateEventModelArgumentCaptor.getValue();
 
         Assertions.assertEquals("NOTIFY_TO_CREATE_HERO", heroCreateEventModel.getEventType());
+    }
+
+    @Test
+    void shouldBeReturnAllHeroes() {
+        var heroes = Heroes.builder()
+                .id(UUID.randomUUID())
+                .alignment(HeroAlignmentEnum.HERO)
+                .name("Spider Man")
+                .power("Wall-crawling, web-slinging, spider-sense")
+                .build();
+
+        when(heroRepository.findAll()).thenReturn(List.of(heroes));
+
+        var firstHeroResponse = heroService.getHeroes().get(0);
+
+        Assertions.assertNotNull(firstHeroResponse);
+        Assertions.assertEquals(heroes.getId(), firstHeroResponse.id());
     }
 }
